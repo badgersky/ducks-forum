8c58903cd6318393d00fab69770052dc46198838
badgersky <szymonek.ptasznik@gmail.com>
2023-05-14 20:29:00 +0200

    migrations, add duck form

---
+++ ducks/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+from ducks import models
+
+
+class AddDuckForm(forms.ModelForm):
+    """Add Duck Form"""
+
+    class Meta:
+        model = models.Duck
+        fields = ('name',
+                  'description',
+                  'occupied_areas',
+                  'image',
+                  'avg_height',
+                  'avg_weight',
+                  'strength',
+                  'agility',
+                  'intelligence',
+                  'charisma')
+        
