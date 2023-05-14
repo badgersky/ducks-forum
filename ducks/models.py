75becfad82f5071b28e4025425b1921ec15af938
badgersky <szymonek.ptasznik@gmail.com>
2023-05-14 20:21:11 +0200

    create Duck model, migrate model

--- ducks/models.py
+++ ducks/models.py
@@ -1,3 +1,28 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Duck(models.Model):
+    """model representing duck"""
+
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='ducks')
+    occupied_areas = models.TextField()
+    strength = models.DecimalField(validators=[MinValueValidator(0), MaxValueValidator(10.0)],
+                                   max_digits=3,
+                                   decimal_places=1
+                                   )
+    agility = models.DecimalField(validators=[MinValueValidator(0), MaxValueValidator(10.0)],
+                                  max_digits=3,
+                                  decimal_places=1
+                                  )
+    intelligence = models.DecimalField(validators=[MinValueValidator(0), MaxValueValidator(10.0)],
+                                       max_digits=3,
+                                       decimal_places=1
+                                       )
+    charisma = models.DecimalField(validators=[MinValueValidator(0), MaxValueValidator(10.0)],
+                                   max_digits=3,
+                                   decimal_places=1
+                                   )
+    date_at = models.DateTimeField(auto_now_add=True)
