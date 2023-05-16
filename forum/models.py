a40decf39ccbd6ea784fed181bf5aad2b51e2a92
badgersky <szymonek.ptasznik@gmail.com>
2023-05-16 17:17:45 +0200

    add default fields to models

--- forum/models.py
+++ forum/models.py
@@ -7,7 +7,7 @@ class Thread(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     creator = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
-    likes = models.IntegerField(validators=[MinValueValidator(0)])
+    likes = models.IntegerField(validators=[MinValueValidator(0)], default=0)
     comments = models.ForeignKey('Comment', on_delete=models.CASCADE)
     date_at = models.DateTimeField(auto_now_add=True)
 
@@ -15,7 +15,7 @@ class Thread(models.Model):
 class Comment(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     content = models.TextField()
-    likes = models.IntegerField(validators=[MinValueValidator(0)])
+    likes = models.IntegerField(validators=[MinValueValidator(0)], default=0)
     date_at = models.DateTimeField(auto_now_add=True)
 
 
