208e07c8e4352c1f36c87729af735a072e2fe731
badgersky <szymonek.ptasznik@gmail.com>
2023-05-16 17:16:04 +0200

    create models to manage forum, migrate changes

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Thread(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    creator = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
+    likes = models.IntegerField(validators=[MinValueValidator(0)])
+    comments = models.ForeignKey('Comment', on_delete=models.CASCADE)
+    date_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    content = models.TextField()
+    likes = models.IntegerField(validators=[MinValueValidator(0)])
+    date_at = models.DateTimeField(auto_now_add=True)
+
+
+class LikeComment(models.Model):
+    comment = models.ForeignKey('Comment', on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+
+class LikeThread(models.Model):
+    thread = models.ForeignKey('Thread', on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
