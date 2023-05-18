825216fcc30edf4290f6eb246ddb8d629a57a067
badgersky <szymonek.ptasznik@gmail.com>
2023-05-18 17:00:55 +0200

    create del fav duck view, endpoint, linked view to button

--- users/urls.py
+++ users/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('add-fav-duck/<int:pk>/', views.AddFavDuck.as_view(), name='add-fav-duck'),
+    path('del-fav-duck/<int:pk>/', views.DelFavDeck.as_view(), name='del-fav-duck'),
 ]
