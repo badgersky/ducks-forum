d972fc72997923a911f8dac659a9fe15e506e51b
badgersky <szymonek.ptasznik@gmail.com>
2023-05-19 15:01:57 +0200

    update home view, sort threads by likes

--- home/views.py
+++ home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views import View
 
+from forum.models import Thread
+
 
 class HomeView(View):
 
     def get(self, request):
-        return render(request, 'home/home.html')
+        threads = Thread.objects.all().order_by('-likes')
+        return render(request, 'home/home.html', {'threads': threads})
