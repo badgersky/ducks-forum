804e56adc81db8c5f3e43c19c2fefd667eff27a3
badgersky <szymonek.ptasznik@gmail.com>
2023-05-20 13:15:14 +0200

    update conftest.py, add registration and login test for method post

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ import pytest
 def user(db, django_user_model):
     """User instance"""
 
-    user = django_user_model.create_user(
+    user = django_user_model.objects.create_user(
         username='username1',
         password='password1',
         )
--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -17,3 +17,39 @@ def test_login_get(db, client):
 
     assert response.status_code == 200
     assert '<h2 class="border-bottom border-top border-black p-2">Login</h2>' in response.content.decode('utf-8')
+
+
+def test_registration_post(db, django_user_model, client):
+    url = reverse('users:register')
+    data = {
+        'username': 'username1',
+        'password': 'password1',
+        'confirm_password': 'password1',
+        }
+
+    redirect = client.post(url, data)
+    user = django_user_model.objects.get(username=data['username'])
+
+    response = client.get(redirect.url)
+
+    assert redirect.status_code == 302
+    assert user.username == data['username']
+    assert response.status_code == 200
+    assert 'Registration Successful, please login' in response.content.decode('utf-8')
+
+
+def test_login_post(user, db, client):
+    url = reverse('users:login')
+    data = {
+        'username': 'username1',
+        'password': 'password1',
+        }
+
+    redirect = client.post(url, data)
+
+    response = client.get(redirect.url)
+
+    assert redirect.status_code == 302
+    assert response.status_code == 200
+    assert '<h2 class="border-bottom border-top border-black p-2">Most popular threads</h2>' in \
+           response.content.decode('utf-8')
