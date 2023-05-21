9688836c0b8a68011339701bd6760a74cfdb7f67
badgersky <szymonek.ptasznik@gmail.com>
2023-05-21 13:04:41 +0200

    correct models

--- ducks/models.py
+++ ducks/models.py
@@ -12,22 +12,26 @@ class Duck(models.Model):
     origin_country = models.CharField(max_length=200, blank=True)
     avg_weight = models.DecimalField(validators=[MinValueValidator(0)], max_digits=3, decimal_places=1)
     user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
-    strength = models.DecimalField(validators=[MinValueValidator(0), MaxValueValidator(10.0)],
-                                   max_digits=3,
-                                   decimal_places=1
-                                   )
-    agility = models.DecimalField(validators=[MinValueValidator(0), MaxValueValidator(10.0)],
-                                  max_digits=3,
-                                  decimal_places=1
-                                  )
-    intelligence = models.DecimalField(validators=[MinValueValidator(0), MaxValueValidator(10.0)],
-                                       max_digits=3,
-                                       decimal_places=1
-                                       )
-    charisma = models.DecimalField(validators=[MinValueValidator(0), MaxValueValidator(10.0)],
-                                   max_digits=3,
-                                   decimal_places=1
-                                   )
+    strength = models.DecimalField(
+        validators=[MinValueValidator(0), MaxValueValidator(10.0)],
+        max_digits=3,
+        decimal_places=1
+    )
+    agility = models.DecimalField(
+        validators=[MinValueValidator(0), MaxValueValidator(10.0)],
+        max_digits=3,
+        decimal_places=1
+    )
+    intelligence = models.DecimalField(
+        validators=[MinValueValidator(0), MaxValueValidator(10.0)],
+        max_digits=3,
+        decimal_places=1
+    )
+    charisma = models.DecimalField(
+        validators=[MinValueValidator(0), MaxValueValidator(10.0)],
+        max_digits=3,
+        decimal_places=1
+    )
     date_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -37,10 +41,11 @@ class Duck(models.Model):
 class DuckRate(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
     duck = models.ForeignKey('Duck', on_delete=models.CASCADE)
-    rate = models.DecimalField(validators=[MinValueValidator(0), MaxValueValidator(10.0)],
-                               max_digits=3,
-                               decimal_places=1)
+    rate = models.DecimalField(
+        validators=[MinValueValidator(0), MaxValueValidator(10.0)],
+        max_digits=3,
+        decimal_places=1
+    )
 
     def __str__(self):
         return f'{self.duck.name} - {str(self.rate)}'
-    
