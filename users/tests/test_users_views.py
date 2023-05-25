e85caecd34d00b967394f8a0b680de2dc2b4d2a9
badgersky <szymonek.ptasznik@gmail.com>
2023-05-25 13:01:08 +0200

    test add fav duck with no permission, moved tests to proper file

--- users/tests/test_users_views.py
+++ users/tests/test_users_views.py
@@ -52,3 +52,54 @@ def test_login_post(user, db, client):
     assert redirect.status_code == 302
     assert response.status_code == 200
     assert f'<span class="navbar-text m-2">Logged in {data["username"]}</span>' in response.content.decode('utf-8')
+
+
+def test_add_fav_duck(client, db, duck, user):
+    url = reverse('users:add-fav-duck', kwargs={'pk': duck.pk})
+    client.force_login(user)
+    fav_ducks_num = user.fav_ducks.count()
+
+    redirect = client.get(url)
+    fav_ducks_num_after = user.fav_ducks.count()
+    fav_duck = user.fav_ducks.first()
+
+    response = client.get(redirect.url)
+
+    assert redirect.status_code == 302
+    assert response.status_code == 200
+    assert fav_ducks_num == fav_ducks_num_after - 1
+    assert fav_duck.name == duck.name
+
+
+def test_delete_fav_duck(client, db, user, duck):
+    user.fav_ducks.add(duck)
+    url = reverse('users:del-fav-duck', kwargs={'pk': duck.pk})
+    client.force_login(user)
+    fav_ducks_num = user.fav_ducks.count()
+
+    redirect = client.get(url)
+    fav_ducks_num_after = user.fav_ducks.count()
+
+    response = client.get(redirect.url)
+
+    assert response.status_code == 200
+    assert redirect.status_code == 302
+    assert fav_ducks_num == fav_ducks_num_after + 1
+
+
+def test_add_duck_no_permission(client, db):
+    redirect, response = _test_not_logged_user(client, reverse('ducks:add'))
+
+    assert '<h2 class="border-bottom border-top border-black p-2">Login</h2>' in response.content.decode('utf-8')
+
+
+def test_add_fav_duck_no_permission(client, db, duck):
+    redirect, response = _test_not_logged_user(client, reverse('users:add-fav-duck', kwargs={'pk': duck.pk}))
+
+    assert '<h2 class="border-bottom border-top border-black p-2">Login</h2>' in response.content.decode('utf-8')
+
+
+def _test_not_logged_user(client, url):
+    redirect = client.get(url)
+    response = client.get(redirect.url)
+    return redirect, response
